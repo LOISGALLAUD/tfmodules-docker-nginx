c4ffb44dfd0f751fa8c4ed2d659b14356655e874
LOISGALLAUD <loisgallaud@hotmail.com>
2024-07-11 11:54:32 +0200

    add: docker installation module

---
+++ main.tf
@@ -0,0 +1,10 @@
+variable "ssh_host" {}
+variable "ssh_user" {}
+variable "ssh_key" {}
+
+module "docker" {
+  source   = "./modules/docker"
+  ssh_host = var.ssh_host
+  ssh_user = var.ssh_user
+  ssh_key  = var.ssh_key
+}
